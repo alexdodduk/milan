1c257226994da89592bee1ddb613df493d911645
Alex Dodd <adodd@live.co.uk>
2015-05-18 20:20:39 +0100

    Allowed HTML to be passed over as the About.Body

--- Alix Milan/Alix Milan/Models/About.cs
+++ Alix Milan/Alix Milan/Models/About.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Alix_Milan.Models
 {
@@ -11,6 +12,7 @@ namespace Alix_Milan.Models
         public int ID { get; set; }
 
         [Required]
+        [AllowHtml]
         [DataType(DataType.MultilineText)]
         public string Body { get; set; }
 
